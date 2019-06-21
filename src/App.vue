300ec10418c52536c6d59da18ff00dd0cc3d6fdb
Daniel Mack <daniel@zonque.org>
2019-06-21 20:58:05 +0200

    ajax fiddling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BlaBla :data="data" :year="year" />
+    <button @click="loadData()">reload data</button>
+    <button @click="increaseYear()">increase year</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import HelloWorld from './components/HelloWorld.vue'
+import BlaBla from './components/BlaBla.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    BlaBla,
+  },
+  data() {
+    return {
+      data: {},
+      year: 2010
+    }
+  },
+  methods: {
+    loadData() {
+      axios.get('/data.json').then((response) => {
+        this.data = response.data;
+        console.log("Reloaded!!");
+      });
+    },
+    increaseYear() {
+      this.year++;
+    }
+  },
+  mounted() {
+    this.loadData();
   }
 }
 </script>
---
+++ src/components/BlaBla.vue
@@ -0,0 +1,35 @@
+<template>
+        <div>
+                <h1>{{ data.one }}</h1>
+                <h1>{{ data.two }}</h1>
+                <p>Year: {{ year }}</p>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'BlaBla',
+  props: {
+    data: Object,
+    year: Number
+  },
+  watch: {
+          data: function(val) {
+                  this.makePlot();
+          },
+          year: function(val) {
+                  this.makePlot();
+          }
+  },
+  mounted() {
+  },
+  methods: {
+     makePlot() {
+          console.log("MAKE PLOT for year", this.year, this.data);
+     }
+  }
+}
+</script>
+
+<style scoped>
+</style>
